891a9fdb18e8e7a34255b5991bf2e08d33d5d1d6
raul_cgk <raulcgk93@gmail.com>
2021-06-16 17:29:12 -0500

    added tic-tac-toe board design function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,2 +1,13 @@
-print("hello_world")
-lst = []
+def tic_tac_toe(value_lst):
+    print("     |     |     ")
+    print("  {}  |  {}  |  {} ".format(value_lst[0], value_lst[1], value_lst[2]))
+    print("_____|_____|_____")
+    print("     |     |     ")
+    print("  {}  |  {}  |  {} ".format(value_lst[3], value_lst[4], value_lst[5]))
+    print("_____|_____|_____")
+    print("     |     |     ")
+    print("  {}  |  {}  |  {} ".format(value_lst[6], value_lst[7], value_lst[8]))
+    print("     |     |     ")
+    
+
+tic_tac_toe(list(range(1,10)))
